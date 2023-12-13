f867053d914ced8030dbd679652865e4a98b40a2
Irvan Zidny <74554600+Rgoptimus@users.noreply.github.com>
2023-12-13 16:36:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,18 @@
 import streamlit as st
 
-def calculate_sgpa(grades):
-    credit_points = {"B": 4, "C": 3.5, "D": 3, "E": 2.5, "F": 2, "G": 1}
-
-    total_credit = 0
-    total_marks = 0
-
-    for grade in grades:
-        credit = credit_points.get(grade, 0)
-        total_credit += credit
-        total_marks += credit * 10
-
-    sgpa = total_marks / len(grades) / 10
-    return sgpa
-
 def main():
-    st.title("SGPA Calculator")
+    st.title("Formulir Data Pengguna")
 
-    # Input for grades
-    grades = st.text_input("Enter grades separated by spaces (e.g., B C D):").split()
+    # Pertanyaan Usia
+    usia = st.slider("Berapa usia Anda?", min_value=0, max_value=100, value=25)
 
-    if grades:
-        # Calculate SGPA
-        sgpa = calculate_sgpa(grades)
+    # Pertanyaan Jenis Kelamin
+    jenis_kelamin_options = ["Laki-laki", "Perempuan"]
+    jenis_kelamin = st.selectbox("Jenis kelamin Anda apa?", jenis_kelamin_options)
 
-        # Display results
-        st.success(f"SGPA: {sgpa:.2f}")
+    # Menampilkan hasil
+    st.write(f"Usia: {usia} tahun")
+    st.write(f"Jenis Kelamin: {jenis_kelamin}")
 
 if __name__ == "__main__":
     main()
