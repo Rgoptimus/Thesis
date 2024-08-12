bb872525c520d09a38cd1f81832363ec52b69411
Irvan Zidny <74554600+Rgoptimus@users.noreply.github.com>
2024-08-12 23:26:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,15 @@ import requests
 from transformers import pipeline
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import subprocess
+
+
+# Establishing a google sheet connection
+conn = st.experimental_connection("gsheets", type = GSheetsConnection)
+# Fetch existing vendors data
+existing_data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1z7UPC-LoZDsNvsVbGv4cYsAM8D65wdIKci3xBXGMTqw/edit?usp=sharing", usecols=list(range(3)), ttl=1)
 
 
 # Function to load the model from a pickle file
